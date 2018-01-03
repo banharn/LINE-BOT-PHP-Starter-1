0f633f54b775a21807ba481d0a5d84fb1b9847b5
banharn <iicekub@hotmail.com>
2018-01-03 13:22:47 +0700

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -18,7 +18,7 @@
   
 <div class="container">
 <div class="panel panel-success">
-      <div class="panel-heading"><label>Store</label></div>
+      <div class="panel-heading">Store</div>
       <div class="panel-body">
         
         <div class="form-group">

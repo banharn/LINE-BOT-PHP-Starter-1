8cb32f71e79b2ed34b23babbe5bc97413eb86604
banharn <iicekub@hotmail.com>
2018-01-03 13:39:30 +0700

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -1,75 +1,65 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <style>
-  .container {
-    padding-top: 20px;
-}
-   </style>
-</head>
-<body>
-  
-<div class="container">
-<div class="panel panel-success">
-      <div class="panel-heading" style="font-size:20px;font-weight:700;">Store</div>
-      <div class="panel-body">
-        
-        <div class="form-group">
-      <label for="inputdefault">Barcode Number :</label>
-      <input class="form-control" id="inputdefault" type="text"  value="<?php echo $_GET['idBarC']; ?>" disabled>
-    </div>
-        
-        <div class="form-group">
-      <label for="inputdefault">รหัสสินค้า :</label>
-      <input class="form-control" id="inputdefault" type="text"   >
-    </div>
-        
-        <div class="form-group">
-      <label for="inputdefault">ชื่อสินค้า :</label>
-      <input class="form-control" id="inputdefault" type="text"   >
-    </div>
-        
-        <div class="form-group">
-      <label for="inputdefault">น้ำหนัก :</label>
-      <input class="form-control" id="inputdefault" type="text" >
-    </div>
-       
-        <div class="form-group">
-      <label for="inputdefault">ชื่อ Supplier :</label>
-      <input class="form-control" id="inputdefault" type="text"   >
-    </div>
-        
-          <div class="form-group">
-      <label for="inputdefault">จำนวน :</label>
-      <input class="form-control" id="inputdefault" type="text"   >
-    </div>
-                     
-           <div class="form-group">
-  <label for="cause">สาเหตุ :</label>
-  <textarea class="form-control" rows="2" id="cause"></textarea>
-</div>
-        
-           <div class="form-group">
-  <label for="PO">PO :</label>
-  <textarea class="form-control" rows="2" id="PO"></textarea>
-</div>
-        
-</div>
-<div class="panel-footer " style="height: 50px;">
-	<div class="pull-right">
-	<button type="submit" id="save" class="btn btn-primary btn-sm">
-	<span class="glyphicon glyphicon-ok"></span> บันทึก</button>	
-</div>
-</div>
-  </div>
-    </div>
-</body>
+   <head>
+      <title>Bootstrap Example</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <style>
+         .container {
+         padding-top: 20px;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="container">
+         <div class="panel panel-success">
+            <div class="panel-heading" style="font-size:20px;font-weight:700;">Store</div>
+            <div class="panel-body">
+               <div class="form-group">
+                  <label for="inputdefault">Barcode Number :</label>
+                  <input class="form-control" id="inputdefault" type="text"  value="<?php echo $_GET['idBarC']; ?>" disabled>
+               </div>
+               <div class="form-group">
+                  <label for="inputdefault">รหัสสินค้า :</label>
+                  <input class="form-control" id="inputdefault" type="text"   >
+               </div>
+               <div class="form-group">
+                  <label for="inputdefault">ชื่อสินค้า :</label>
+                  <input class="form-control" id="inputdefault" type="text"   >
+               </div>
+               <div class="form-group">
+                  <label for="inputdefault">น้ำหนัก :</label>
+                  <input class="form-control" id="inputdefault" type="text" >
+               </div>
+               <div class="form-group">
+                  <label for="inputdefault">ชื่อ Supplier :</label>
+                  <input class="form-control" id="inputdefault" type="text"   >
+               </div>
+               <div class="form-group">
+                  <label for="inputdefault">จำนวน :</label>
+                  <input class="form-control" id="inputdefault" type="text"   >
+               </div>
+               <div class="form-group">
+                  <label for="cause">สาเหตุ :</label>
+                  <textarea class="form-control" rows="2" id="cause"></textarea>
+               </div>
+               <div class="form-group">
+                  <label for="PO">PO :</label>
+                  <textarea class="form-control" rows="2" id="PO"></textarea>
+               </div>
+            </div>
+            <div class="panel-footer ">
+               <div class="pull-right">
+                  <button type="submit" id="save"
+                     class="btn btn-primary btn-sm">
+                  <span class="glyphicon glyphicon-ok"></span> บันทึก
+                  </button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
 </html>
-

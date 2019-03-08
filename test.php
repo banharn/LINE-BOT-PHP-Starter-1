0baf8955d59e66ab7f03406142065c3db101d8fb
banharn <iicekub@hotmail.com>
2019-03-08 11:35:17 +0700

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 $url = 'https://ph.godaddy.com/hosting/website-builder.aspx?ci=88060';
 $contents = htmlentities(file_get_contents($url));
- $arrayJson = json_decode($content, true);
+ $arrayJson = json_decode($contents, true);
     
     $arrayHeader = array();
     $arrayHeader[] = "Content-Type: application/json";

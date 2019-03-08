21a9f702e59f97d80028fe55d6f17e99207a6612
banharn <iicekub@hotmail.com>
2019-03-08 15:28:46 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://192.168.0.4:2145/Reprogram/Default.aspx" ;
+$url = "http://1.179.149.85:2146/Reprogram/Default.aspx" ;
 $param = "?" . "Field1=" . $num . "&Field2=" . $name;
 
 $curl = curl_init();

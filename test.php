45ddfa6c0113036d5377d14b1532b06c37b8e403
banharn <iicekub@hotmail.com>
2019-03-09 10:49:39 +0700

    Update test.php

--- test.php
+++ test.php
@@ -19,8 +19,7 @@
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close ($ch);
-$apiResult = json_decode($result, true);
-echo $apiResult;
-
+        echo $result;
+        echo "<script>console.log( 'Debug Objects: " . $result . "' );</script>";
    exit;
 ?>

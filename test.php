7455374792bd0de60ff24eab1ddcb4d8c0106814
banharn <iicekub@hotmail.com>
2019-03-09 10:33:53 +0700

    Update test.php

--- test.php
+++ test.php
@@ -16,7 +16,6 @@
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);

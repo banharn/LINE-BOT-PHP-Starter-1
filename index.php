ac2bbe0bbb109301819f4f4601ab0b08b51739fc
banharn <iicekub@hotmail.com>
2019-03-09 08:59:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
     
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
-$id = $arrayJson['events'][0]['source']['userId'];
-$displayName = $arrayJson['events'][0]['source']['displayName'];
+
 $baseUrl = "http://1.179.149.85:2146/register/default2.aspx";
 $resource = "?serial=$message";
   $ch = curl_init(); 
@@ -27,7 +26,7 @@ $resource = "?serial=$message";
 
  $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] =$displayName;
+        $arrayPostData['messages'][0]['text'] =$output;
         replyMsg($arrayHeader,$arrayPostData);
 
 #ตัวอย่าง Message Type "Text"

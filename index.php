6a2976e99e2910b090c254cef3fc7c324d85d0b1
banharn <iicekub@hotmail.com>
2019-03-09 09:17:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,16 +24,6 @@ $resource = "?serial=$message";
         curl_close($ch);  
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('VQ1mBEd2QqIIBJwg629MTQCf3uTJjOMgZXp+ZHvBP9Znn07x3HkiMiUk7GCcwhD/R6VI1s2Nhc31rKx6ElxmT26P2Ve2oWqc7KK9dZaDC1coQQxoVlck0Kydnq6UaC0JhBSJa275g99+OxBmaXGdDAdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '5959f81685d08ed63ba53cede92d678d']);
-$response = $bot->getProfile('$id');
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo $profile['displayName'];
-    echo $profile['pictureUrl'];
-    echo $profile['statusMessage'];
-}
-
 
  $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";

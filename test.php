cdba168338e7d22732828fe9f5bfaa0786449fc1
banharn <iicekub@hotmail.com>
2019-03-09 10:53:27 +0700

    Update test.php

--- test.php
+++ test.php
@@ -19,7 +19,7 @@
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close ($ch);
-        echo $result;
-        echo "<script>console.log( 'Debug Objects: " . $result . "' );</script>";
+        echo $result[displayName];
+       
    exit;
 ?>

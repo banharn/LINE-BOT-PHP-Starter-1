f34009c27f89083cdec7ab0a923430ffa1d724f2
banharn <iicekub@hotmail.com>
2019-03-09 08:49:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $resource = "?serial=$message";
 
  $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = $output+" "+$id;
+        $arrayPostData['messages'][0]['text'] =$id;
         replyMsg($arrayHeader,$arrayPostData);
 
 #ตัวอย่าง Message Type "Text"

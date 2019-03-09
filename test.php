76cd36ea6f752aabdd8dabeb6aacb62fdb039832
banharn <iicekub@hotmail.com>
2019-03-09 09:42:59 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,24 @@
 <?php
-
-  // create curl resource 
-        $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, "http://1.179.149.85:2146/register/default2.aspx"); 
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        // close curl resource to free up system resources 
-        curl_close($ch);   
-	echo $output;
+$accessToken = "VQ1mBEd2QqIIBJwg629MTQCf3uTJjOMgZXp+ZHvBP9Znn07x3HkiMiUk7GCcwhD/R6VI1s2Nhc31rKx6ElxmT26P2Ve2oWqc7KK9dZaDC1coQQxoVlck0Kydnq6UaC0JhBSJa275g99+OxBmaXGdDAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    
+    //รับข้อความจากผู้ใช้
+$id = "Ub50d949097ea4a3a880d45d26389fb95";
+ $bot = new \\LINE\LINEBot(new CurlHTTPClient('$accessToken'), \[
+ 'channelSecret' => '070f839dfd1f720350258656368bef4f'
+\]);
+ 
+$res = $bot->getProfile('$id');
+if ($res->isSucceeded()) {
+ $profile = $res->getJSONDecodedBody();
+ $displayName = $profile\['displayName'\];
+ $statusMessage = $profile\['statusMessage'\];
+ $pictureUrl = $profile\['pictureUrl'\];
+}
 ?>

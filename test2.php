ad4981df63e3c26af2acd1818b60241264356d13
banharn <iicekub@hotmail.com>
2019-03-11 15:49:30 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,5 +1,6 @@
 <?php
-        $fileURL = 'http://1.179.149.85:2146/register/Images/test.txt;
+$file_name = "data.txt";
+        $fileURL = 'http://1.179.149.85:2146/register/Images/test.txt';
 		$fp = fopen($fileURL, 'w');
 		fwrite($fp, "dddddd");
 		fclose($fp);		

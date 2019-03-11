9dbf6b9bfa2c78c031465a4be750cf1725882ac1
banharn <iicekub@hotmail.com>
2019-03-11 11:31:49 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,4 +1,49 @@
 <?php
-$str1 = "ᶤᶜᵉ ᴷᵘᶰᵍ";
-echo urlencode($str1) ;
+    $accessToken = "VQ1mBEd2QqIIBJwg629MTQCf3uTJjOMgZXp+ZHvBP9Znn07x3HkiMiUk7GCcwhD/R6VI1s2Nhc31rKx6ElxmT26P2Ve2oWqc7KK9dZaDC1coQQxoVlck0Kydnq6UaC0JhBSJa275g99+OxBmaXGdDAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];
+    $messageID = $arrayJson['events'][0]['message']['id'];
+    $messagePIC = $arrayJson['events'][0]['message']['contentProvider']['originalContentUrl'];
+    $messagePIC1 = $arrayJson['events'][0]['message']['originalContentUrl'];
+    $id = $arrayJson['events'][0]['source']['userId'];
+    $groupId = $arrayJson['events'][0]['source']['groupId'];
+    //$strUrl = "https://api.line.me/v2/bot/profile/$id";
+   // $strUrl = "https://api.line.me/v2/bot/group/$groupId/member/$id";
+$strUrl = "https://api.line.me/v2/bot/message/9493595447603/content";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+        $character = json_decode($result);
+        $displayName = $character->displayName;  
+        $str1 = urlencode($displayName);
+echo    $strUrl;
+
+
+    function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+   exit;
 ?>

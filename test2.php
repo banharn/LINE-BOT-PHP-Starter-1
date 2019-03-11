b3ed49fc7a732df7c47be359f24b1aa32adccdeb
banharn <iicekub@hotmail.com>
2019-03-11 15:29:29 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,41 +1,6 @@
 <?php
-    $accessToken = "VQ1mBEd2QqIIBJwg629MTQCf3uTJjOMgZXp+ZHvBP9Znn07x3HkiMiUk7GCcwhD/R6VI1s2Nhc31rKx6ElxmT26P2Ve2oWqc7KK9dZaDC1coQQxoVlck0Kydnq6UaC0JhBSJa275g99+OxBmaXGdDAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-    
-    $content = file_get_contents('php://input');
-    $arrayJson = json_decode($content, true);
-    
-
-    $arrayHeader = array();
-    //$arrayHeader[] = "Content-Type: application/json";
-    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-    //รับข้อความจากผู้ใช้
-
-
- $baseUrl = "https://api.line.me/v2/bot/message/9493595447603/content";
-    $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, "$baseUrl"); 
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        // close curl resource to free up system resources 
-        curl_close($ch);  
-echo $output;
-
-    function replyMsg($arrayHeader,$arrayPostData){
-        $strUrl = "https://api.line.me/v2/bot/message/reply";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$strUrl);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close ($ch);
-    }
-   exit;
+        $fileURL = 'http://1.179.149.85:2146/register/Images/'.$objID.'.'.$filenameExtension;
+		$fp = fopen($fileURL, 'w');
+		fwrite($fp, $json_content);
+		fclose($fp);		
 ?>

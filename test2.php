e956711eef30bacf2178f78b915bd6c4e6164ed5
banharn <iicekub@hotmail.com>
2019-03-11 13:19:04 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -12,9 +12,12 @@
 
 $strUrl = "https://api.line.me/v2/bot/message/9493595447603/content";
  $ch = curl_init();
+        $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);

8b28f47f8b0a1c8360e698211fd726633f102d2b
banharn <iicekub@hotmail.com>
2019-03-14 16:47:03 +0700

    Update linehook.php

--- linehook.php
+++ linehook.php
@@ -38,9 +38,71 @@
     if($messagePIC == "text"){
 	if(is_numeric ($message))
 	{
-       	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = $output1;
+       //	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+       // $arrayPostData['messages'][0]['type'] = "text";
+        //$arrayPostData['messages'][0]['text'] = $output1;
+	$arrayPostData = '{
+  "type": "bubble",
+  "styles": {
+    "footer": {
+      "separator": true
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Register Program SK v.9",
+        "weight": "bold",
+        "color": "#1DB446",
+        "size": "sm"
+      },
+      {
+        "type": "text",
+        "text": "SK ให้บริการ24ชม.คับ",
+        "weight": "bold",
+        "size": "xl",
+        "margin": "md"
+      },
+      {
+        "type": "separator",
+        "margin": "xxl"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "xxl",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "รหัสลงทะเบียน",
+                "weight": "bold",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "01888817",
+                "weight": "bold",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}';	
         replyMsg($arrayHeader,$arrayPostData);
 	}
 	else

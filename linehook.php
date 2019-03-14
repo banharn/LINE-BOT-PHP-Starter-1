2f98917dbc94913e63f79a02a5ea620597592911
banharn <iicekub@hotmail.com>
2019-03-14 16:24:28 +0700

    Create linehook.php

---
+++ linehook.php
@@ -0,0 +1,77 @@
+<?php
+    $accessToken = "VQ1mBEd2QqIIBJwg629MTQCf3uTJjOMgZXp+ZHvBP9Znn07x3HkiMiUk7GCcwhD/R6VI1s2Nhc31rKx6ElxmT26P2Ve2oWqc7KK9dZaDC1coQQxoVlck0Kydnq6UaC0JhBSJa275g99+OxBmaXGdDAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];
+    $messageID = $arrayJson['events'][0]['message']['id'];
+    $messagePIC =  $arrayJson['events'][0]['message']['type'];
+    $id = $arrayJson['events'][0]['source']['userId'];
+    $groupId = $arrayJson['events'][0]['source']['groupId'];
+    	//$strUrl = "https://api.line.me/v2/bot/profile/$id";
+    	$strUrl = "https://api.line.me/v2/bot/group/$groupId/member/$id";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+        $character = json_decode($result);
+        $displayName = $character->displayName;  
+        $str1 = urlencode($displayName);
+   	$baseUrl = "http://1.179.149.85:2146/register/default2.aspx";
+    	$resource = "?serial=$message&name=$str1";
+    	$ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL, "$baseUrl$resource"); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        $output = curl_exec($ch); 
+        curl_close($ch);  
+    	$output1 = "ไลน์ผู้ใช้งาน : $displayName\nรหัสลงทะเบียน : $output";
+    if($messagePIC == "text"){
+	if(is_numeric ($message))
+	{
+       	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = $output1;
+        replyMsg($arrayHeader,$arrayPostData);
+	}
+	else
+	{}
+    } else if($messagePIC == "image"){
+	replyMsgs($arrayHeader,$messageID);
+    }
+    function replyMsgs($arrayHeader,$messageID){
+	$ch = curl_init("https://api.line.me/v2/bot/message/$messageID/content");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader); 
+	$result = curl_exec($ch);
+	curl_close($ch);
+	$file_name = "$messageID.jpg";
+        $file_name = "ftp://meengineer:OC7IuVdsGP@ftp.meengineer.co.th/domains/meengineer.co.th/public_html/images/reg/$file_name";
+	$fp = fopen($file_name, 'wb');
+	fwrite($fp, $result);
+	fclose($fp);
+    }
+    function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+   exit;
+?>

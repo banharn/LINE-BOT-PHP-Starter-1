b9e968b096ac42385a4bc9bcd45aeb0cb6a9947c
banharn <iicekub@hotmail.com>
2019-03-15 14:45:47 +0700

    Update linehook.php

--- linehook.php
+++ linehook.php
@@ -36,7 +36,7 @@
         curl_close($ch);  
     	$output1 = "ไลน์ผู้ใช้งาน : $displayName\nรหัสลงทะเบียน : $output\n $id\n $groupId";
 $json = '{
-    "to": $groupId,
+    "to": "C7317fb64845b1e89c63c4fb86bafb268",
     "messages":[{
        "type": "flex",
     "altText": "Register Program SK V.92",

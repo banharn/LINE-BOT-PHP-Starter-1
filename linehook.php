4c9e1c7a92d5db416192233f84cd14e27ab350e2
banharn <iicekub@hotmail.com>
2019-03-15 14:31:09 +0700

    Update linehook.php

--- linehook.php
+++ linehook.php
@@ -36,74 +36,7 @@
         curl_close($ch);  
     	$output1 = "ไลน์ผู้ใช้งาน : $displayName\nรหัสลงทะเบียน : $output\n $id\n $groupId";
 
-$test = {
-       "type": "flex",
-    "altText": "Register Program SK V.9",
-    "contents": 
-    
-    {
-  "type": "bubble",
-  "styles": {
-    "footer": {
-      "separator": true
-    }
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Register Program SK v.9",
-        "weight": "bold",
-        "color": "#1DB446",
-        "size": "sm"
-      },
-      {
-        "type": "text",
-        "text": "SK ให้บริการ24ชม.คับ",
-        "weight": "bold",
-        "size": "xl",
-        "margin": "md"
-      },
-      {
-        "type": "separator",
-        "margin": "xxl"
-      },
-      {
-        "type": "box",
-        "layout": "vertical",
-        "margin": "xxl",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "รหัสลงทะเบียน",
-                "weight": "bold",
-                "size": "sm",
-                "color": "#555555",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "01888817",
-                "weight": "bold",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-}
-    };
+
     if($messagePIC == "text"){
 	if(is_numeric ($message))
 	{

993bd405a02e31e37c374e9b806d26f6c167f562
banharn <iicekub@hotmail.com>
2019-03-15 11:57:51 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,4 +3,6 @@
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "test2333";
 echo $arrayPostData;
+
+echo "<script>console.log('$arrayPostData');</script>";
 ?>

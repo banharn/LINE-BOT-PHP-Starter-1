0fa9026c3411c143d67f7a70ea62467be98b2950
banharn <iicekub@hotmail.com>
2019-03-18 15:07:44 +0700

    Update ftp.php

--- ftp.php
+++ ftp.php
@@ -5,8 +5,9 @@ $baseUrl = "http://1.179.149.85:2146/services/service1.aspx";
         curl_setopt($ch, CURLOPT_URL, "$baseUrl"); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         $output = curl_exec($ch);
+        $arr2 = "'.$output.'";
         $arr1 = json_decode($output, true);
-        $arr2 = '.$arr1.';
+        
         curl_close($ch);  
 
 echo $arr2;

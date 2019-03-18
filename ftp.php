81e940f8d48ec9914c2817680516a4dfef6b7a08
banharn <iicekub@hotmail.com>
2019-03-18 14:57:58 +0700

    Update ftp.php

--- ftp.php
+++ ftp.php
@@ -5,15 +5,14 @@ $baseUrl = "http://1.179.149.85:2146/services/service1.aspx";
         curl_setopt($ch, CURLOPT_URL, "$baseUrl"); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         $output = curl_exec($ch);
-        $arrayJson = json_encode($output);
         curl_close($ch);  
          
-echo $arrayJson;
-$arr = json_decode($output, true);
-echo $arr["id"]."<br/>";
+echo $output;
+
 
 $age = '{"Poll":55,"Devid":40,"Akbar":68,"Cally":70}';
 $arr = json_decode($age, true);
 echo $arr["Poll"]."<br/>";
+echo $arr->.Poll"<br/>";
 ?>
 

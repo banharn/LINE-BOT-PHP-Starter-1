84b1673888d43cca5f2496e8f3a2ee1ce9192d8c
banharn <iicekub@hotmail.com>
2019-03-18 15:39:45 +0700

    Update ftp.php

--- ftp.php
+++ ftp.php
@@ -4,9 +4,9 @@ $content = file_get_contents('http://1.179.149.85:2146/services/service1.aspx');
 $arrayJson = json_decode($content, true);
 
 echo $arrayJson['results'][0][id];
-foreach($arrayJson as $key=>$value)
+foreach($arrayJson as $key)
 {
-    echo $key . "=>" . $value[0][id] . "<br>";
+    echo $key[0][id] . "<br>";
 }
 ?>
 

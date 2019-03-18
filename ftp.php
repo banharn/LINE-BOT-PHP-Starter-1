0b37f033cbb31ca87650b78cbe0b351b2ebd9f67
banharn <iicekub@hotmail.com>
2019-03-18 14:04:51 +0700

    Update ftp.php

--- ftp.php
+++ ftp.php
@@ -1,58 +1,7 @@
 <?php 
 
-$con=ftp_connect("ftp.meengineer.co.th"); 
 
-if($con) 
-{ 
-    echo"connected"; 
-} 
-else 
-{ 
-    echo "disconnectd"; 
-} 
-                 
-$con_login=ftp_login($con,"meengineer","OC7IuVdsGP");     
-
-if($con_login) 
-{ 
-    echo "login"; 
-} 
-
-else 
-{ 
-    echo"not login"; 
-
-} 
-
-
-$contents=ftp_nlist($con,"public_html/foldername");  //working  
-print_r($contents); 
-
-
-
-$ret=ftp_nb_get($con,"c:\image.jpg","public_html/foldername/image.jpg",FTP_BINARY); //working 
-
-while ($ret == FTP_MOREDATA) 
-{ 
-   
-   // Do whatever you want 
-   echo "."; 
-
-   // Continue downloading... 
-   $ret = ftp_nb_continue($con); 
-} 
-
-if ($ret != FTP_FINISHED) 
-{ 
-   echo "There was an error downloading the file..."; 
-   exit(1); 
-} 
-
-if ($ret == FTP_FINISHED) 
-{ 
-    echo "downloaded"; 
-} 
-
-ftp_close($con); 
-
+    $content = file_get_contents('http://1.179.149.85:2146/services/service1.aspx');
+    $arrayJson = json_decode($content, true);
+    echo $arrayJson;
 ?>
